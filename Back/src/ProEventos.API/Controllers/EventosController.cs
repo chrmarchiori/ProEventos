d80d3835265012e18e2bd1e339ce108aa3a0c57f
chrmarchiori <chrmarchiori@hotmail.com>
2022-04-07 10:29:21 -0300

    Dividido a aplicação em camadas

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ProEventos.API.Data;
-using ProEventos.API.Models;
+using ProEventos.Persistence;
+using ProEventos.Domain;
 
 namespace ProEventos.API.Controllers
 {
@@ -13,8 +13,8 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventosController : ControllerBase
     {
-        private readonly DataContext __context;
-        public EventosController(DataContext context)
+        private readonly ProEventosContext __context;
+        public EventosController(ProEventosContext context)
         {
             this.__context = context;
         }
@@ -28,7 +28,7 @@ namespace ProEventos.API.Controllers
         [HttpGet("{id}")]
         public Evento GetById(int id)
         {
-            return __context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+            return __context.Eventos.FirstOrDefault(evento => evento.Id == id);
         }
 
         [HttpPost]
---
+++ Back/src/ProEventos.Application/EventoService.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Threading.Tasks;
+using ProEventos.Application.Contratos;
+using ProEventos.Domain;
+using ProEventos.Persistence.Contratos;
+
+namespace ProEventos.Application
+{
+    public class EventoService : IEventoService
+    {
+        private readonly IGeralPersist _geralPersist;
+        private readonly IEventoPersist _eventoPersist;
+
+        public EventoService(IGeralPersist geralPersist, IEventoPersist eventoPersist)
+        {
+            this._eventoPersist = eventoPersist;
+            this._geralPersist = geralPersist;
+        }
+
+        public async Task<Evento> AddEventos(Evento model)
+        {
+            try
+            {
+                _geralPersist.Add<Evento>(model);
+                if (await _geralPersist.SaveChangesAsync())
+                {
+                    return await _eventoPersist.GetAllEventosByIdAsync(model.Id, false);
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Evento> UpdateEvento(int eventoId, Evento model)
+        {
+            try
+            {
+                var evento = await _eventoPersist.GetAllEventosByIdAsync(eventoId, false);
+                if (evento == null) return null;
+
+                model.Id = evento.Id;
+
+                _geralPersist.Update(model);
+
+                if (await _geralPersist.SaveChangesAsync())
+                {
+                    return await _eventoPersist.GetAllEventosByIdAsync(model.Id, false);
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {                
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> DeleteEvento(int eventoId)
+        {
+            try
+            {
+                var evento = await _eventoPersist.GetAllEventosByIdAsync(eventoId, false);
+                if (evento == null) throw new Exception("Evento para delete não encontrado");
+
+                _geralPersist.Delete<Evento>(evento);
+                return await _geralPersist.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {                
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Task<Evento[]> GetAllEventosAsync(bool includePalestrantes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Evento> GetAllEventosByIdAsync(int EventoId, bool includePalestrantes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        
+    }
+}
